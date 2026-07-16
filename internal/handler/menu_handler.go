d1ea34dc60c01c9c2155455efc8d66032833cb62
Rey <syahfauzi73@rocketmail.com>
2026-07-16 15:07:21 +0700

    feat: mock add user

--- internal/handler/menu_handler.go
+++ internal/handler/menu_handler.go
@@ -1,19 +1,68 @@
 package handler
 
 import (
+	"bufio"
 	"fmt"
+	"gym-cli/internal/domain"
+	"strconv"
+	"strings"
 )
 
 type Menu struct {
-	// userHandler domain.UserHandler
+	userHandler domain.UserHandler
+	reader      *bufio.Reader
 }
 
-func NewMenu() *Menu {
+func NewMenu(uh domain.UserHandler, reader *bufio.Reader) *Menu {
 	return &Menu{
-		// userHandler: uh,
+		userHandler: uh,
+		reader:      reader,
 	}
 }
 
 func (a *Menu) Run() {
-	fmt.Println("Hello, World!")
+
+	for {
+		fmt.Println("\033[0;33m========================================")
+		fmt.Println("     GOLD'S GYM MEMBERSHIP SYSTEM")
+		fmt.Print("========================================\033[0m")
+		fmt.Printf(`
+1. Add New Member
+2. View All Members
+3. Update Members
+4. Delete Member
+
+0. Exit Application
+
+Choose Menu: `)
+
+		menuInput, err := a.reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("\n\033[0;31mUnexpected error occured when reading input.\033[0m", err)
+			return
+		}
+
+		menuSelection, err := strconv.Atoi(strings.TrimSpace(menuInput))
+
+		if err != nil {
+			fmt.Println("\n\033[0;31mInvalid input, try again.\n\033[0m")
+			continue
+		}
+
+		exitApp := false
+		switch menuSelection {
+		case 1:
+			a.userHandler.Create()
+		case 0:
+			exitApp = true
+		default:
+			fmt.Println("\n\033[0;31mInvalid input, try again.\n\033[0m")
+			continue
+		}
+
+		if exitApp {
+			fmt.Println("Exiting Application.")
+			break
+		}
+	}
 }
--- internal/handler/user_handler.go
+++ internal/handler/user_handler.go
@@ -1,17 +1,109 @@
 package handler
 
-import "gym-cli/internal/domain"
+import (
+	"bufio"
+	"fmt"
+	"gym-cli/internal/domain"
+	"net/mail"
+	"strconv"
+	"strings"
+)
 
 type userHandler struct {
-	uc domain.UserUsecase
+	uc     domain.UserUsecase
+	reader *bufio.Reader
 }
 
-func NewUserHandler() domain.UserHandler {
-	return &userHandler{}
+func NewUserHandler(reader *bufio.Reader) domain.UserHandler {
+	return &userHandler{
+		reader: reader,
+	}
 }
 
 func (h *userHandler) Create() {
+	for {
+		fmt.Println("\n\033[0;33m========== ADD NEW MEMBER ==========\033[0m")
+		fmt.Print("Email: ")
+		emailInput, err := h.reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("\n\033[0;31mUnexpected error occured when reading input.\033[0m", err)
+			continue
+		}
 
+		email := strings.TrimSpace(emailInput)
+		addr, err := mail.ParseAddress(email)
+		if err != nil || addr.Address != email {
+			fmt.Println("\n\033[0;31mInvalid email format, please try again.\033[0m")
+			continue
+		} else if email == "test@mail.com" {
+			// TODO: ADD DUPLICATE CHECKER
+			fmt.Println("\n\033[0;31mEmail is taken, please enter another email.\033[0m")
+			continue
+		}
+
+		var tier string
+		for {
+			// TODO: PRINT TIERS DYNAMICALLY
+			fmt.Printf(`
+Tiers:
+1. Gold
+2. Silver
+3. Bronze	
+				
+Select tier: `)
+			tierInput, err := h.reader.ReadString('\n')
+			if err != nil {
+				fmt.Println("\n\033[0;31mUnexpected error occured when reading input.\033[0m", err)
+				continue
+			}
+			tierSelection, err := strconv.Atoi(strings.TrimSpace(tierInput))
+			if err != nil {
+				fmt.Println("\n\033[0;31mInvalid input, try again.\033[0m")
+				continue
+			}
+
+			switch tierSelection {
+			case 1:
+				tier = "Gold"
+			case 2:
+				tier = "Silver"
+			case 3:
+				tier = "Bronze"
+			default:
+				fmt.Println("\n\033[0;31mInvalid input, try again.\033[0m")
+				continue
+			}
+			break
+		}
+
+		// TODO: ADD INSERT USER
+		fmt.Println("\n\033[0;32mNew Member added.\033[0m")
+		fmt.Println("Email:", email)
+		fmt.Println("Tier:", tier)
+
+		anotherMember := false
+		for {
+			fmt.Print("\nWould you like to add another member? (yes/no) ")
+			cont, err := h.reader.ReadString('\n')
+			cont = strings.ToLower(strings.TrimSpace(cont))
+			if err != nil {
+				fmt.Println("\n\033[0;31mUnexpected error occured when reading input.\033[0m", err)
+				continue
+			}
+
+			if strings.ToLower(cont) == "yes" {
+				anotherMember = true
+			} else if strings.ToLower(cont) != "no" {
+				fmt.Println("\n\033[0;31mInvalid input, try again.\033[0m")
+				continue
+			}
+			break
+		}
+		if anotherMember {
+			continue
+		}
+		break
+	}
 }
 
 func (h *userHandler) List() {
