7ba9e1b6b4f1f483c2d401c19dad204b71bbe39a
Rey <syahfauzi73@rocketmail.com>
2026-07-17 11:16:47 +0700

    fix: context deadline exceeded

--- internal/usecase/user_usecase.go
+++ internal/usecase/user_usecase.go
@@ -17,7 +17,7 @@ func NewUserUsecase(repo domain.UserRepository) domain.UserUsecase {
 	}
 }
 
-var setTimeout = 3 * time.Millisecond
+var setTimeout = 3 * time.Second
 
 func (u *userUsecase) Create(
 	email, password, firstName, lastName, address string,
